a634f1d3d6ae98f156555927980e97296ee85c7c
mhmdka <moh.kahani@gmail.com>
2020-01-26 17:27:11 +0330

    phase-2 init

---
+++ phase-2/R/phase-2.R
@@ -0,0 +1,16 @@
+
+# Title: Phase 1 Data Mining
+# Authors: Ali Goldani 9512762107
+#         Mohammad Kahani 9512762447
+
+# Libraries
+library("ggpubr")
+library("dplyr")
+library("ltm")
+library("caret")
+library("FactoMineR") # for PCA function
+library("clusterSim")
+library("caTools")
+library(randomForest)
+library(MASS)
+library(ggbiplot)
